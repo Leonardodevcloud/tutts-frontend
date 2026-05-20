e02f7e54ae24e09a72990566f27b1e631211d7b3
Leonardodevcloud <leonardosuporteflash@gmail.com>
2026-05-20 09:52:02 -0300

    fix: sigla Motofrete C corrigida para DC (codigo real da API Tutts)

--- modulo-config.js
+++ modulo-config.js
@@ -1644,7 +1644,7 @@
                 var TODAS_CATS = [
                     {sigla: "M",  nome: "Motofrete"},
                     {sigla: "MC", nome: "Motofrete (Expresso)"},
-                    {sigla: "C",  nome: "Motofrete C"},
+                    {sigla: "DC", nome: "Motofrete C"},
                     {sigla: "U",  nome: "Carro Utilitário"},
                     {sigla: "UC", nome: "Carro Utilitário (Expresso)"},
                     {sigla: "D",  nome: "Tutts Fast"}
